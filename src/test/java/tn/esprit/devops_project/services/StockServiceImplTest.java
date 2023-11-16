2990c56707320f68a0318fca2121e5216e7b2562
Synda Mejri <synda.mejri@esprit.tn>
2023-11-16 08:22:52 +0100

    updates

--- src/test/java/tn/esprit/devops_project/services/StockServiceImplTest.java
+++ src/test/java/tn/esprit/devops_project/services/StockServiceImplTest.java
@@ -13,18 +13,45 @@ import java.util.List;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class StockServiceImplTest {
+    @Mock
+    private StockRepository stockRepository;
 
+    @InjectMocks
+    private StockServiceImpl stockService;
 
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
     @Test
     void addStock() {
+        // Mocking behavior
+        Stock stockToAdd = new Stock();
+        when(stockRepository.save(any(Stock.class))).thenReturn(stockToAdd);
 
+        // Testing the service method
+        Stock addedStock = stockService.addStock(stockToAdd);
+
+        // Verification
+        assertNotNull(addedStock);
+        assertEquals(stockToAdd, addedStock);
     }
 
 
     @Test
     void retrieveAllStock() {
+        // Mocking behavior
+        when(stockRepository.findAll()).thenReturn(List.of(new Stock(), new Stock()));
+
+        // Testing the service method
+        List<Stock> stocks = stockService.retrieveAllStock();
+
+        // Verification
+        assertNotNull(stocks);
+        assertEquals(2, stocks.size());
 
     }
 }
